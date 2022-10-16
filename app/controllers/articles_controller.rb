1f064f2ef3ab6d8f1541fabca22d10130e8a8530
Elli <papaioannou.elli@gmail.com>
2022-10-16 18:03:52 +0300

    Added return links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,7 @@
 class ArticlesController < ApplicationController
 
     def show
-    
-    @article = Article.find(params[:id])
+        @article = Article.find(params[:id])
     end
     
 
@@ -11,12 +10,37 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
         @article = Article.new(params.require(:article).permit(:title,:description))
-        @article.save
-        redirect_to @article
+        if  @article.save
+          flash[:notice] = "Article was created successfully."
+          redirect_to @article
+        else
+            render 'new'
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if  @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+             render 'edit'
+        end  
     end
 
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+
+    end
 end
