dbecc02351bf9d7d0ed79cdd84ba3e492d41f93e
Elli <papaioannou.elli@gmail.com>
2022-10-20 19:51:44 +0300

    Clean out layout

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
- validates :title, presence: true, length: {minimum:6, maximum:100}
- validates :description, presence: true, length: {minimum:6, maximum:200}
+ validates :title, presence: true, length: {minimum:2, maximum:1000000}
+ validates :description, presence: true, length: {minimum:2, maximum:2000000}
 end
